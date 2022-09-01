4099e2a4f76ad2ddcf28c86dd3f9abd0eda50b45
홍성표 <sungpyo9053@naver.com>
2022-09-01 14:52:24 +0900

    [홍성표, 0, NONE][제목] 스프링부트 시작하기
    [원인]
    -
    
    [개선내역]
    -
    
    [영향도]
    1. 사이드이펙트 가능성: 없음
    2. 타플랫폼 영향성: 없음
    3. 타향지 영향성 : 없음
    
    [변경점 체크리스트]
    -

---
+++ spring/webproject-board/src/main/java/com/webproject/webprojectboard/WebProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.webproject.webprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProjectBoardApplication.class, args);
+	}
+
+}
---
+++ spring/webproject-board/src/test/java/com/webproject/webprojectboard/WebprojectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webproject.webprojectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
